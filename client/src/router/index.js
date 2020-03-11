74d3c39252911f690e62400c3d01dbafff283b18
sanjay reddy m <msanjayreddy007@gmail.com>
2020-03-11 05:14:37 -0400

    commit

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-import { CurrentUser } from '../models/Users';
 
 
 
